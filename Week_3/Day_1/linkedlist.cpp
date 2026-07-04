4becc42d8b8c84c4a6554abf220414f1393995cb
Sakshi-Gupta242 <sakshigu24@gmail.com>
2026-07-04 18:47:38 +0530

    commit

--- Week_3/Day_1/linkedlist.cpp
+++ Week_3/Day_1/linkedlist.cpp
@@ -1,57 +1,74 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-class ListNode {
-public:
+// Definition of ListNode
+struct ListNode {
     int val;
     ListNode* next;
 
     ListNode(int x) {
         val = x;
-        next = NULL;
+        next = nullptr;
     }
 };
 
-bool hasCycle(ListNode* head) {
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-    if (head == NULL || head->next == NULL)
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+                return true;
+        }
+
         return false;
-
-    ListNode* slow = head;
-    ListNode* fast = head;
-
-    while (fast != NULL && fast->next != NULL) {
-
-        slow = slow->next;
-        fast = fast->next->next;
-
-        if (slow == fast)
-            return true;
     }
-
-    return false;
-}
+};
 
 int main() {
+    int n;
+    cout << "Enter number of nodes: ";
+    cin >> n;
 
-    // Create nodes
-    ListNode* head = new ListNode(3);
-    ListNode* second = new ListNode(2);
-    ListNode* third = new ListNode(0);
-    ListNode* fourth = new ListNode(-4);
+    if (n == 0) {
+        cout << "Linked List is empty." << endl;
+        return 0;
+    }
 
-    // Link nodes
-    head->next = second;
-    second->next = third;
-    third->next = fourth;
+    vector<ListNode*> nodes;
 
-    // Create cycle (tail -> second node)
-    fourth->next = second;
+    cout << "Enter node values: ";
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        nodes.push_back(new ListNode(x));
+    }
 
-    if (hasCycle(head))
-        cout << "Cycle Detected";
+    // Connect nodes normally
+    for (int i = 0; i < n - 1; i++) {
+        nodes[i]->next = nodes[i + 1];
+    }
+
+    int pos;
+    cout << "Enter position to create cycle (-1 for no cycle): ";
+    cin >> pos;
+
+    if (pos != -1) {
+        nodes[n - 1]->next = nodes[pos];
+    }
+
+    Solution obj;
+
+    if (obj.hasCycle(nodes[0]))
+        cout << "Cycle Detected" << endl;
     else
-        cout << "No Cycle";
+        cout << "No Cycle" << endl;
 
     return 0;
 }
