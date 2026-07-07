10ff8e4681069fdc3510dff8b8c358157f7f3290
Sakshi-Gupta242 <sakshigu24@gmail.com>
2026-07-07 16:36:08 +0530

    commit

--- Week_2/oops/Saxophone.java
+++ Week_2/oops/Saxophone.java
@@ -1,3 +1,11 @@
-public class Saxophone {
-    
+package music.wind;
+
+import music.Playable;
+
+public class Saxophone implements Playable {
+
+    @Override
+    public void play() {
+        System.out.println("Playing Saxophone");
+    }
 }
