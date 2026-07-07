7157c17f86f8bc043464a0465ace23e1f8405524
Sakshi-Gupta242 <sakshigu24@gmail.com>
2026-07-07 16:34:54 +0530

    commit

--- Week_2/oops/Veena.java
+++ Week_2/oops/Veena.java
@@ -1,3 +1,11 @@
-public class Veena {
-    
+package music.string;
+
+import music.Playable;
+
+public class Veena implements Playable {
+
+    @Override
+    public void play() {
+        System.out.println("Playing Veena");
+    }
 }
