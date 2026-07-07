0a366698a57dde9a25b6d75faee7c89259f62265
Sakshi-Gupta242 <sakshigu24@gmail.com>
2026-07-07 16:04:52 +0530

    commit

---
+++ Week_4/day_4/pathsum.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+// Definition of TreeNode
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) {
+        val = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+
+        if (root == nullptr)
+            return false;
+
+        // Leaf node
+        if (root->left == nullptr && root->right == nullptr)
+            return (targetSum == root->val);
+
+        return hasPathSum(root->left, targetSum - root->val) ||
+               hasPathSum(root->right, targetSum - root->val);
+    }
+};
+
+int main() {
+
+    // Tree:
+    //          5
+    //        /   \
+    //       4     8
+    //      /     / \
+    //     11    13  4
+    //    /  \         \
+    //   7    2         1
+
+    TreeNode* root = new TreeNode(5);
+    root->left = new TreeNode(4);
+    root->right = new TreeNode(8);
+
+    root->left->left = new TreeNode(11);
+    root->left->left->left = new TreeNode(7);
+    root->left->left->right = new TreeNode(2);
+
+    root->right->left = new TreeNode(13);
+    root->right->right = new TreeNode(4);
+    root->right->right->right = new TreeNode(1);
+
+    int targetSum = 22;
+
+    Solution obj;
+
+    if (obj.hasPathSum(root, targetSum))
+        cout << "Path Exists";
+    else
+        cout << "Path Does Not Exist";
+
+    return 0;
+}
